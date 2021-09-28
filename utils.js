302780ff133be96b3e22e8e0756791ee24a5e44f
Monsterxiao <monsterxiao@users.noreply.github.com>
2021-09-28 17:35:15 +0800

    updated

--- utils.js
+++ utils.js
@@ -1,11 +1,3 @@
 const log = console.log.bind(console)
 
-// commonjs
-module.exports = {
-    // 'log': log,
-    log,
-}
-
-// es6 导出模块
-// export default log
-// export { log }
+export default log
